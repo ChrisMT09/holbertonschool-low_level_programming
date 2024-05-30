ba5e909fae7543a8852f74c3b852f2e0c3b51ebf
ChrisMT09 <christophermoralestorres@gmail.com>
2024-05-30 15:44:39 +0000

    imprimiendo los numeros del 1 al 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Start pointer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n = 0;
+for (; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
