a94ddb3910fde6c7e4a7a8cdaee8fc5621cb6521
ChrisMT09 <christophermoralestorres@gmail.com>
2024-05-30 21:07:47 +0000

    imprimiendo los numeros del 1 al 9 separados por una , y un espacio, y utilizando solo la funcion de PUTCHAR

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int a;
-unsigned char b;
 for (a = 0; a <= 9; a++)
 {
-putchar('0' + a)
-}
-for (b = 'a'; b <= 'f'; b++)
+putchar('0' + a);
+if (a != 9)
 {
-putchar(b)
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
