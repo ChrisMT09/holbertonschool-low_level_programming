6688dcfbc99be9a5b70014e28ffe6d90af8f83ae
ChrisMT09 <christophermoralestorres@gmail.com>
2024-05-30 16:04:54 +0000

    imprimimos los numeros usando la puncion putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Start pointer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int c = 0;
+for (; c <= 9; c++)
+{
+putchar('0' + c);
+}
+putchar('\n');
+return (0);
+}
