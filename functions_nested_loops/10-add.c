c2c0b59ea5eac5f18ef4f166a83a7307dd8be6cf
ChrisMT09 <christophermoralestorres@gmail.com>
2024-06-01 16:26:16 +0000

    multiplicando 2 numeros enteros y devilviendo el resultado

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Start pointer
+ * @a: first number to sumer
+ * @b: second number to sumer
+ * Return: print to a + b
+ */
+
+int add(int a, int b)
+{
+int s = a + b;
+return (s);
+}
